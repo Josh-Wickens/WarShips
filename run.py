1d37c138d675f4566692445e659516a92fb62368
Josh-Wickens <joshwickens@hotmail.com>
2022-06-08 08:46:53 +0000

    add intro to WarShips and username entry

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-
-
-print("Welcome to Warships!")
+"""
+Intro to WarShips and enter players username
+"""
+print("\nWelcome to Warships!")
+print("\nPlease tell me your name")
+print("\nBefore we get started, I need to know your name \n")
+username = input("\ninsert name: ")
+print(f"\nWelcome to WarShips {username}!")
