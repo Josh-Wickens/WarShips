312c5fc88f156129b9ac580682b3cd2d2dafe418
Josh-Wickens <joshwickens@hotmail.com>
2022-06-10 11:04:21 +0000

    create ship position for horizontal

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 # Import random so that the random function can be used
-import random
+from random import randint
 
 
 # Intro to WarShips and enter players username
@@ -17,17 +17,34 @@ def welcome():
 # Calls the welcome message function
 welcome()
 
-# Build board for the game using 0 for placements
+
 def build_board(dims):
+    # Build board for the game using 0 for placements
     return [['O' for count in range(dims)] for count in range(dims)]
 
 
 board = build_board(6)
 
 def print_board(board):
-    # This function will get rid of the commas and brackets for the board
+    '''
+    This function will get rid of the commas, quotations 
+    and brackets for the board.
+    The * makes the list on one line
+    '''
     for b in board:
         print(*b)
 
 
+def build_ship(dims):
+    len_ship = random.randint(2, dims)
+    orientation = random.randint(0, 1)
+    if orientation == 0:
+        row_ship = [random.randint(0, dims - 1)] * len_ship
+        col = random.randint(0, dims - len_ship)
+        col_ship = list(range(col, col + len_ship))
+        coords = tuple(zip(row_ship, col_ship))
+    
+
 print_board(board)
+
+
